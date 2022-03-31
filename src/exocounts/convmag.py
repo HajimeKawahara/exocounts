e7f71a7b68a0639f7013c61649bc696f59b2ed1f
Hajime Kawahara <divrot@gmail.com>
2022-04-01 08:45:14 +0900

    refactorring of nstar

--- src/exocounts/convmag.py
+++ src/exocounts/convmag.py
@@ -9,46 +9,51 @@ from astropy import units as u
 
 
 def get_magdict(maglist=None):
-    magdict={}
+    magdict = {}
     if(maglist is None):
-        maglist="../data/mag.list"
+        maglist = '../data/mag.list'
 
 #    print "Read "+maglist
 
-    maglist=pd.read_csv(maglist,delimiter=',')
-    
+    maglist = pd.read_csv(maglist, delimiter=',')
+
     return maglist
 
-def get_flux(band,mag,magdict):
-    mask=magdict["band"]==band
-    a = float(magdict["a"][mask].values[0])
-    flux=10**(a - 0.4*mag)*u.erg/u.s/(u.cm)**2/u.micron
+
+def get_flux(band, mag, magdict):
+    mask = magdict['band'] == band
+    a = float(magdict['a'][mask].values[0])
+    flux = 10**(a - 0.4*mag)*u.erg/u.s/(u.cm)**2/u.micron
     return flux
 
-def get_mag(band,flux,magdict):
-    mask=magdict["band"]==band
-    a = float(magdict["a"][mask].values[0])
-    fluxcgs=flux.to(u.erg/u.s/(u.cm)**2/u.micron).value
-    mag=2.5*(a - np.log10(fluxcgs))
+
+def get_mag(band, flux, magdict):
+    mask = magdict['band'] == band
+    a = float(magdict['a'][mask].values[0])
+    fluxcgs = flux.to(u.erg/u.s/(u.cm)**2/u.micron).value
+    mag = 2.5*(a - np.log10(fluxcgs))
 
     return mag
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Convert magnitude to flux based on Traub and Oppenheimer')
-    parser.add_argument('-b', nargs=1,required=True,help='Choose band from U,B,V,R,I,J,H,Ks,L,M,N,Q')
-    parser.add_argument('-m', nargs=1,required=True,help='magnitude' )
-    args = parser.parse_args()    
 
-    band=args.b[0]
-    mag=float(args.m[0])
-    print("Band=",band," Magnitude=",mag)
-    magdict=get_magdict()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Convert magnitude to flux based on Traub and Oppenheimer')
+    parser.add_argument('-b', nargs=1, required=True,
+                        help='Choose band from U,B,V,R,I,J,H,Ks,L,M,N,Q')
+    parser.add_argument('-m', nargs=1, required=True, help='magnitude')
+    args = parser.parse_args()
+
+    band = args.b[0]
+    mag = float(args.m[0])
+    print('Band=', band, ' Magnitude=', mag)
+    magdict = get_magdict()
     print(magdict)
-    mask=magdict["band"]==band
+    mask = magdict['band'] == band
 
-    print("center=",magdict["lambda0"][mask].values[0],"micron")
-    print("width=",magdict["dlambda"][mask].values[0],"micron")
-    print(get_flux(band,mag,magdict))
-    flux=get_flux(band,mag,magdict)
-    print(get_flux(band,mag,magdict).to(u.erg/u.s/u.m/u.m/u.nm))
-    print(get_mag(band,flux,magdict))
+    print('center=', magdict['lambda0'][mask].values[0], 'micron')
+    print('width=', magdict['dlambda'][mask].values[0], 'micron')
+    print(get_flux(band, mag, magdict))
+    flux = get_flux(band, mag, magdict)
+    print(get_flux(band, mag, magdict).to(u.erg/u.s/u.m/u.m/u.nm))
+    print(get_mag(band, flux, magdict))
