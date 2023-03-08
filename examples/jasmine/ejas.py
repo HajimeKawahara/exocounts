945b476c1001ad43ac90711fd3d7d52fa0af7384
Hajime Kawahara <divrot@gmail.com>
2023-03-08 14:09:59 +0900

    update JASMINE current value

--- examples/jasmine/ejas.py
+++ examples/jasmine/ejas.py
@@ -5,19 +5,19 @@ from astropy import units as u
 import numpy as np
 
 ejas=exocounts.InstClass()
-ejas.lamb = 1.25*u.micron #micron
-ejas.dlam = 0.7*u.micron #micron
-#ejas.lamb = 1.3*u.micron #micron
-#ejas.dlam = 0.6*u.micron #micron
+#ejas.lamb = 1.25*u.micron #micron
+#ejas.dlam = 0.7*u.micron #micron
+ejas.lamb = 1.3*u.micron #micron
+ejas.dlam = 0.6*u.micron #micron
 
-ejas.dtel = 0.34*u.m #telescope diameter m
-ejas.dstel = 0.14*u.m #secondary telescope diameter m or 12.4 (3 tels)
+ejas.dtel = 0.36*u.m #telescope diameter m
+ejas.dstel = 0.126*u.m #secondary telescope diameter m or 12.4 (3 tels)
 
-QE=0.7
+QE=0.76
 ejas.throughput = QE*0.85*0.95
-ejas.ndark = 15.5/u.s #dark current
+ejas.ndark = 25.5/u.s #dark current
 ejas.nread = 15.0 #nr
-ejas.fullwell = 150000.
+ejas.fullwell = 100000.
 
 target=exocounts.TargetClass()
 target.teff = 3000.0*u.K #K
